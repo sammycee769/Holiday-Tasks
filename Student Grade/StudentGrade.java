f42d835d49e5c81139750c19281a6ea52f8d1b0b
christian <samuelchristian769@gmail.com>
2026-01-10 10:37:18 +0100

    add

--- Student Grade/StudentGrade.java
+++ Student Grade/StudentGrade.java
@@ -8,10 +8,15 @@ public class StudentGrade {
 
         System.out.print("Enter number of subjects: ");
         int subjects = input.nextInt();
+        
+        System.out.println("\nSaving ................ ");
+        System.out.println("Saved succesfully");
 
         int[][] scores = new int[students][subjects];
+        int[] totals = new int[students];
+        double[] averages = new double[students];
+        int[] position = new int[students];
 
-        // Collect scores
         for (int count = 0; count < students; count++) {
             System.out.println("\nEntering scores for Student " + (count + 1));
 
@@ -32,26 +37,48 @@ public class StudentGrade {
                 scores[count][counter] = score;
             }
         }
+         for (int count = 0; count < students; count++) {
+                    int sum = 0;
+                    for (int counter = 0; counter < subjects; counter++) {
+                        sum += scores[count][counter];
+                    }
+                    totals[count] = sum;
+                    averages[count] = (double) sum / subjects;
+                }
 
+       for (int positionCount = 0; positionCount < students; positionCount++) {
+            position[positionCount] = 1;
+            for (int positionCounter = 0; positionCounter < students; positionCounter ++) {
+                if (totals[positionCounter] > totals[positionCount]) {
+                    position[positionCount]++;
+                }
+            }
+                }
 
-        System.out.println("\nCLASS SUMMARY");
+        System.out.println("=".repeat(60));
+        System.out.printf("%-10s","STUDENT");
 
+        for (int count = 0; count < subjects; count++) {
+            System.out.printf(" %5s", "SUB" + (count + 1));
+        }
+        System.out.printf(" %6s %7s %5s%n", "TOT", "AVE", "POS");
+        System.out.println("=".repeat(60));
         for (int count = 0; count < students; count++) {
-            int total = 0;
+            System.out.printf("%-10s", "Student " + (count + 1));
 
             for (int counter = 0; counter < subjects; counter++) {
-                total += scores[count][counter];
+                System.out.printf(" %5d", scores[count][counter]);
             }
 
-            double average = (double) total / subjects;
-
-            System.out.println(
-                "Student " + (count + 1) +
-                " | Total: " + total +
-                " | Average: " + String.format("%.2f", average)
+            System.out.printf(
+                " %6d %7.2f %5d%n",
+                totals[count],
+                averages[count],
+                position[count]
             );
         }
 
-    }
+        System.out.println("=".repeat(60));
+}
 }
 
